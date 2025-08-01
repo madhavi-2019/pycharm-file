9eff790aa85562c5c3a3789f325c59dfd00bec18
madhavi-2019 <madhavikashibai12@gmail.com>
2025-08-01 22:22:27 +0530

    Create ftl.py

---
+++ ftl.py
@@ -0,0 +1,13 @@
+from math import factorial
+x=int(input("enter the number"))
+
+factorial(x)
+if x<5:
+
+    'return 1'
+
+else:
+    'return'*(factorial(x-1))
+
+result = factorial(5)
+print(result)
